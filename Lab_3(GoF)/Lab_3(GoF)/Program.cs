644f2b02a026d1ec9afb36c3ef7df7e2eccef035
Ilyak16 <kupriyanovin@mer.ci.nsu.ru>
2026-03-03 09:47:15 +0700

    GoF(Lab3)

---
+++ Lab_3(GoF)/Lab_3(GoF)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_3_GoF_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
